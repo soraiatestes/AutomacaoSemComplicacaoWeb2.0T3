84ce8c15868f81234bb45bed5228b056a1cffce6
Soraia testes <soraiasantos3@hotmail.com>
2025-01-10 11:24:18 -0300

    aula 23 e 24, printScreen e headless

--- src/test/java/br/com/chronosacademy/core/Driver.java
+++ src/test/java/br/com/chronosacademy/core/Driver.java
@@ -2,48 +2,85 @@ package br.com.chronosacademy.core;
 
 import br.com.chronosacademy.enums.Browser;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Driver {
     private static WebDriver driver;
     private static WebDriverWait wait;
+    private static String nomeCenario;
+    private static File diretorio;
+    private static int numPrint;
+
+    public static File getDiretorio() {
+        return diretorio;
+    }
+
+    public static String getNomeCenario() {
+        return nomeCenario;
+    }
+
+    public static void setNomeCenario(String nomeCenario) {
+        Driver.nomeCenario = nomeCenario;
+    }
 
     public Driver(Browser navegador){
         switch (navegador){
-            case CHROME:
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
+              case CHROME:
+                  startChrome();
+                  break;
             case IE:
-                WebDriverManager.iedriver().setup();
-                driver = new InternetExplorerDriver();
+                startIE();
                 break;
             case FIREFOX:
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                startFirefox();
                 break;
             case EDGE:
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            default:
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                startEdge();
                 break;
         }
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
 
+    }
+
+    private static void startEdge() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+    }
+
+    private static void startFirefox() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    private static void startIE() {
+        WebDriverManager.iedriver().setup();
+        driver = new InternetExplorerDriver();
+    }
+
+//  private static void startChrome(){
+//    ChromeOptions options = new ChromeOptions();
+//    boolean headless = Boolean.parseBoolean(System.getProperty("headless"));
+//    options.addArguments("--headless=new");
+//    options.addArguments("window-size=1200,720");
+//    driver = new ChromeDriver(options);
+//}
+
+    private static void startChrome(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     public static WebDriver getDriver() {
@@ -61,6 +98,21 @@ public class Driver {
     public static void attributChange(WebElement element, String atribute, String value){
         wait.until(ExpectedConditions.attributeContains(element, atribute, value));
 
+    }
+
+    public static void criaDiretorio(){
+        String caminho = "src/test/resources/evidencias";
+        diretorio = new File(caminho +"/"+ nomeCenario);
+        diretorio.mkdir();
+        numPrint = 0;
+    }
+
+    public static void printScreen(String passo) throws IOException {
+        numPrint = numPrint + 1;
+// ou pode ser: numPrint++;
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String caminho = diretorio.getPath() + "/" + numPrint +" - " + passo + ".png";
+        FileUtils.copyFile(file, new File(caminho));
     }
 
 
